f8e894353f5498477f8987488076d0bf712423de
NManage2 <nmanagel@gmail.com>
2022-07-06 13:04:09 +0200

    amazocomApp

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazocom</a>
+      </header>
+      <main>Liste de Produits</main>
+    </div>
+  );
+}
+
+export default App;
